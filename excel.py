846c521d6fb8f52a0be2e6b73f6112efe1c39df6
Phydon <84594612+Phydon@users.noreply.github.com>
2022-10-22 19:34:32 +0200

    Add files via upload

--- excel.py
+++ excel.py
@@ -3,7 +3,7 @@ from openpyxl import load_workbook
 
 
 def main():
-    workbook = load_workbook(filename="test.xlsx")
+    workbook = load_workbook(filename="C:/Users/Pohl/main/Rust/ata/test.xlsx")
     sheet = workbook.active
 
     max_row = 0
@@ -12,21 +12,23 @@ def main():
 
     sheet.insert_rows(idx=max_row + 1)
 
-    # start = dt.datetime.now()
-    start = dt.datetime(2022,10,22,12,5,30,46000)
+    start = dt.datetime.now()
     date = start.strftime("%d.%m.%Y")
     time = start.strftime("%H:%M")
+    
     sheet.cell(row=max_row + 1, column=1, value=date)
     starttime_cell = sheet.cell(row=max_row + 1, column=2, value=time)
     starttime_loc = starttime_cell.coordinate
     starttime_formula = f"=IF(ISBLANK({starttime_loc}),\"\",{starttime_loc})"
     sheet.cell(row=max_row + 1, column=3, value=starttime_formula)
     
+    random_start = dt.datetime(2022, 10, 22, 12, 10, 23, 1)
+    sheet.cell(row=max_row + 1, column=4, value=random_start.strftime("%H:%M"))
     first_break = dt.datetime.now()
-    delta = round((first_break - start).total_seconds() / (60 * 60), 2)
-    break_cell = sheet.cell(row=max_row + 1, column=3, value=delta)
+    delta = round((first_break - random_start).total_seconds() / (60 * 60), 2)
+    sheet.cell(row=max_row + 1, column=5, value=delta)
         
-    workbook.save(filename="test.xlsx")
+    workbook.save(filename="C:/Users/Pohl/main/Rust/ata/test.xlsx")
     
     
 if __name__ == "__main__":
